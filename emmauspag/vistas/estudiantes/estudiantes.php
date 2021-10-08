1c7ed6913c5e35e51a8bc63f5a7c94bfe0dd0ef9
jandrey <jandrey.serna@utp.edu.co>
2021-10-08 16:27:12 -0500

    plan de estudios evaluando

--- emmauspag/vistas/estudiantes/estudiantes.php
+++ emmauspag/vistas/estudiantes/estudiantes.php
@@ -225,7 +225,7 @@ require_once dirname(dirname(dirname(__FILE__))) . '/modelos/Modelo-estudiantes.
 
 <!-- Modal Plan de estudios -->
 <div class="modal fade" id="plan-estudios" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Plan De Estudios</h5>
