af090252fb0904e44e81c5ddf88323583ef54119
Jose Valencia - Vaio <sejoma989@gmail.com>
2022-09-26 14:57:37 -0500

    feat: creacion de vista administracion, botones

---
+++ emmauspag/vistas/administracion.php
@@ -0,0 +1,25 @@
+<!--  TODO : Administrar estudiante, nuevo estudiante
+ TODO 1: Registrar nuevo curso,registrar nuevo material
+ TODO 2: Registrar proveedor
+ TODO 3: Administrar representantes -->
+
+<div class="admin_contenedor">
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Administrar estudiante    
+    </button>
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Nuevo estudiante
+    </button>
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Registrar nuevo curso
+    </button>
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Registrar nuevo material
+    </button>
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Registrar proveedor
+    </button>
+    <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
+        Administrar representantes
+    </button>
+</div>
