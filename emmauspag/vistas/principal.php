c4d2cb11de1383ac2490b634e20ddcc5494bf841
jose_pro <sejoma989@gmail.com>
2022-11-04 14:50:54 -0500

    feat: organiza pagina principal, estilo

--- emmauspag/vistas/principal.php
+++ emmauspag/vistas/principal.php
@@ -29,37 +29,78 @@
 ?>
 
 <div>
-
     <div class="principal">
         <div>
             <img class="main-image"
                 src="https://instituto.emmauscolombia.com/wp-content/uploads/2021/07/Nuevo-Logo-Jun-2021-01.png"
                 alt="Nuevo logo emmaus">
         </div>
-
-        <div class="admin_contenedor">
-            <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#añadirestudiante">
-                Administrar estudiante
-            </button>
-            <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
-                Nuevo estudiante
-            </button>
-            <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
-                Registrar nuevo curso
-            </button>
-            <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
-                Registrar nuevo material
-            </button>
-            <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#añadirProveedor">
-                Registrar proveedor
-            </button>
-            <button type="button" class="btn btn-outline-success" data-toggle="" data-target="">
-                Administrar representantes
-            </button>
-        </div>
-
     </div>
-
-
-
 </div>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+
+            <!-- Fist card -->
+            <div class="col-md-6 mb-4 text-center">
+                <div class="card h-100">
+                    <h4 class="card-header ">Inicio Estudiantes</h4>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Seccion con las acciones principales, el boton   <span style=" padding: 5px; background-color: green; color: white; "> Estudiantes </span>   
+                            dirige a la administracion de estudiantes. <br> En la seccion del desplegable <span style=" padding: 5px; background-color: #004b7c; color: white; "> > </span> 
+                            hay opciones para <strong> Imprimir certificados </strong>, tambien para <strong>Ver Diplomas</strong>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary waves-effect waves-light">Estudiantes</button>
+                            <button type="button" class="btn btn-secondary dropdown-toggle px-3 waves-effect waves-light"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Imprimir certificado</a>
+                                <a class="dropdown-item" href="#">Ver diplomas</a>
+                                <!-- <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Second card  -->
+            <div class="col-md-6 mb-4 text-center">
+                <div class="card h-100">
+                    <h4 class="card-header">Card 2 Title</h4>
+                    <div class="card-body">
+                        <p class="card-text">Placeholder content for the scrollspy example. If you wanna dance, if you
+                            want
+                            it all, you know that I'm the girl that you should call. Walk through the storm I would. So
+                            let
+                            me get you in your birthday suit. The one that got away.</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary waves-effect waves-light">Primary</button>
+                            <button type="button" class="btn btn-primary dropdown-toggle px-3 waves-effect waves-light"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
