26de8e087b0bbbe16031f2e7faa8f1e13125dcf8
Jose Valencia - VaioSSD <sejoma989@gmail.com>
2022-11-08 20:30:52 -0500

    administracion:  modales proveedores y colabo

--- emmauspag/vistas/administracion.php
+++ emmauspag/vistas/administracion.php
@@ -63,8 +63,7 @@
             <!-- Listar Proveedores -->
             <div class="titulo p-1">
                 <h3>Administracion de Proveedores</h3>
-                <button type="button" class="btn btn-outline-success" data-toggle="modal"
-                    data-target="#añadirestudiante">
+                <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#añadirProveedor">
                     Nuevo Proveedor
                 </button>
             </div>
@@ -103,8 +102,7 @@
             <!-- Listar Colaboradores -->
             <div class="titulo p-1">
                 <h3>Administracion de Colaboradores</h3>
-                <button type="button" class="btn btn-outline-success" data-toggle="modal"
-                    data-target="#añadirestudiante">
+                <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#añadirColaborador">
                     Nuevo Colaborador
                 </button>
             </div>
@@ -142,9 +140,9 @@
 
 </div>
 
+<!-- Modal -->
 
 <!-- Modal crear promotor -->
-<!-- Modal -->
 <div class="modal fade" id="añadirPromotor" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -203,3 +201,125 @@
         </div>
     </div>
 </div>
+
+<!-- Modal crear proveedor -->
+<div class="modal fade" id="añadirProveedor" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Agregar nuevo proveedor</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="content">
+                    <form>
+                        <div class="form-group row">
+                            <label for="inputSelectIglesia">Iglesia</label>
+                            <select id="inputSelectIglesia" class="form-control">
+                                <option selected>Seleccione a la iglesia relacionada</option>
+                                <option>(13) Dosquebradas</option>
+                                <option>(22) Pereira</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputName" class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputName" placeholder="Nombre promotor">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputCiudad" class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputCiudad" placeholder="Ciudad promotor">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputCorreo" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputCorreo" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputTelefono" class="col-sm-2 col-form-label">Telefono</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputTelefono" placeholder="Telefono">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal crear colaborador -->
+<div class="modal fade" id="añadirColaborador" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Agregar nuevo colaborador</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="content">
+                    <form>
+                        <div class="form-group row">
+                            <label for="inputSelectIglesia">Iglesia</label>
+                            <select id="inputSelectIglesia" class="form-control">
+                                <option selected>Seleccione a la iglesia relacionada</option>
+                                <option>(13) Dosquebradas</option>
+                                <option>(22) Pereira</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputName" class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputName" placeholder="Nombre promotor">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputCiudad" class="col-sm-2 col-form-label">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputCiudad" placeholder="Ciudad promotor">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputCorreo" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputCorreo" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputTelefono" class="col-sm-2 col-form-label">Telefono</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputTelefono" placeholder="Telefono">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
