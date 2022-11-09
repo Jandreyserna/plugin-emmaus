444eef8df622c33e9bd16022f70c74b457c6172d
Jose Valencia - VaioSSD <sejoma989@gmail.com>
2022-11-09 16:08:26 -0500

    funcionalidad modal Nuevo Promotor

--- emmauspag/vistas/administracion.php
+++ emmauspag/vistas/administracion.php
@@ -1,5 +1,22 @@
 <?php 
 
+    print_r($_POST);
+    if(!empty($_POST['activo'])){
+        switch ($_POST['activo']) {
+            case 'nuevo-promotor': // accion activada por el boton de añadir un nuevo estudiante de la page estudiante
+                unset($_POST['activo']);
+                foreach($_POST as $campo => $valor) // convierto todas las minusculas a mayusculas
+                {
+                    $_POST[$campo] = 	strtoupper($_POST[$campo]);
+                }
+                // $_POST ['FechaSolicitud'] = date("Y-m-d");
+                // $ultimoId = ultimo_id();
+                // $ultimoId = $ultimoId + 1;
+                // $_POST['IdEstudiante'] = $ultimoId;
+                insert_funtion('promotores', $_POST);
+        }
+    }
+
 
 
 
@@ -165,7 +182,8 @@
             </div>
             <div class="modal-body">
                 <div class="content">
-                    <form>
+                    <form action="" method="post">
+                    <input name="activo" type="hidden" value="nuevo-promotor" >
                         <div class="form-group row">
                             <label for="inputSelectIglesia">Iglesia</label>
                             <select id="inputSelectIglesia" name="IdIglesia" class="form-control">
@@ -184,30 +202,30 @@
                         <div class="form-group row">
                             <label for="inputName" class="col-sm-2 col-form-label">Nombre:</label>
                             <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputName" placeholder="Nombre promotor">
+                                <input type="text" name="Nombre" class="form-control" id="inputName" placeholder="Nombre promotor">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputCiudad" class="col-sm-2 col-form-label">Nombre:</label>
+                            <label for="inputCiudad" class="col-sm-2 col-form-label">Ciudad:</label>
                             <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputCiudad" placeholder="Ciudad promotor">
+                                <input type="text" name="Ciudad" class="form-control" id="inputCiudad" placeholder="Ciudad promotor">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="inputCorreo" class="col-sm-2 col-form-label">Email</label>
                             <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputCorreo" placeholder="Email">
+                                <input type="email" name="CorreoElectronico" class="form-control" id="inputCorreo" placeholder="Email">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="inputTelefono" class="col-sm-2 col-form-label">Telefono</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputTelefono" placeholder="Telefono">
+                                <input type="text" name="Celular" class="form-control" id="inputTelefono" placeholder="Telefono">
                             </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>
 
                     </form>
